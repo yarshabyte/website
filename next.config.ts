9631ee2e159be4d0eaf0ab36799d4da88089bc26
Aashish Chapagain <lalchapagain2014161@gmail.com>
2026-06-05 08:57:14 +0545

    local network settings modified

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  allowedDevOrigins: ["192.168.18.6", "192.168.18.7", "192.168.18.5"],};
+  allowedDevOrigins: ["192.168.18.6", "192.168.18.7", "192.168.18.2"],};
 
 export default nextConfig;
